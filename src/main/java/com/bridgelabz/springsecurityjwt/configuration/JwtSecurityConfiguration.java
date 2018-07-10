b1d7c9e0ef1a3acfa128f0952a187e80cf8a11cf
mamtarao95 <mamtarao9395@gmail.com>
2018-07-10 19:00:46 +0530

    added jwt

--- src/main/java/com/bridgelabz/springsecurityjwt/configuration/JwtSecurityConfiguration.java
+++ src/main/java/com/bridgelabz/springsecurityjwt/configuration/JwtSecurityConfiguration.java
@@ -1,5 +1,43 @@
 package com.bridgelabz.springsecurityjwt.configuration;
 
-public class JwtSecurityConfiguration {
+import java.sql.Date;
 
+import javax.xml.bind.DatatypeConverter;
+
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.springsecurityjwt.models.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtSecurityConfiguration {
+	final static String KEY = "mamta";
+
+	@SuppressWarnings("unused")
+	public String tokenGenerator(User user) {
+		String userName = user.getUserName();
+		String passkey =user.getPassword();
+		long time = System.currentTimeMillis();
+		long nowMillis = System.currentTimeMillis() + (20 * 60 * 60 * 1000);
+		Date now = new Date(nowMillis);
+		JwtBuilder builder = Jwts.builder().setId(passkey).setIssuedAt(now).setSubject(userName)
+				.signWith(SignatureAlgorithm.HS256, KEY);
+		if(time>=0) {
+			
+		}
+		return builder.compact();
+	}
+
+	public void parseJWT(String jwt) {
+
+		Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(KEY)).parseClaimsJws(jwt)
+				.getBody();
+		System.out.println("ID: " + claims.getId());
+		System.out.println("Subject: " + claims.getSubject());
+	}
+	
 }
--- src/main/java/com/bridgelabz/springsecurityjwt/controllers/LoginRegisterationController.java
+++ src/main/java/com/bridgelabz/springsecurityjwt/controllers/LoginRegisterationController.java
@@ -10,46 +10,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.springsecurityjwt.configuration.JwtSecurityConfiguration;
 import com.bridgelabz.springsecurityjwt.models.User;
 import com.bridgelabz.springsecurityjwt.services.UserServiceImplementation;
 import com.bridgelabz.springsecurityjwt.utility.CustomErrorType;
 
 @RestController
 public class LoginRegisterationController {
-	
-   @Autowired
+
+	@Autowired
 	UserServiceImplementation userServiceImplementation;
 
-		@SuppressWarnings({ "rawtypes", "unchecked" })
-		@RequestMapping(value = "/login", method = RequestMethod.POST)
-		public ResponseEntity<User> loginUser(@RequestBody User user) throws ClassNotFoundException, SQLException {
-			System.out.println(user.getUserName()+"iii");
-			if (userServiceImplementation.loginUser(user) == 1) {
-				return new ResponseEntity("Welcome " + user.getUserName(),HttpStatus.OK);
-			}
-			if (userServiceImplementation.loginUser(user) == 0) {
-				return new ResponseEntity(new CustomErrorType("Password is incorrect"), HttpStatus.CONFLICT);
-			}
-			return new ResponseEntity(new CustomErrorType("Username doesnt not exist"), HttpStatus.CONFLICT);
+	@Autowired
+	JwtSecurityConfiguration jwtSecurityConfiguration;
 
-		}
-
-		@SuppressWarnings({ "unchecked", "rawtypes" })
-		@RequestMapping(value = "/register", method = RequestMethod.POST)
-		public ResponseEntity<User> registerUser(@RequestBody User user) throws ClassNotFoundException, SQLException {
-			if (!userServiceImplementation.verifyEmail(user)) {
-				userServiceImplementation.saveUser(user);
-				return new ResponseEntity("User successfully registered", HttpStatus.OK);
-			}
-			return new ResponseEntity(new CustomErrorType("Email-id already exist!!"), HttpStatus.CONFLICT);
-			}
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<User> loginUser(@RequestBody User user) throws ClassNotFoundException, SQLException {
+		System.out.println(user.getUserName() + "iii");
+		if (userServiceImplementation.loginUser(user) == 1) {
+			String token = jwtSecurityConfiguration.tokenGenerator(user);
+			jwtSecurityConfiguration.parseJWT(token);
+			return new ResponseEntity("Welcome " + user.getUserName(), HttpStatus.OK);
 		
-		@RequestMapping("/hi")
-		public String getString() {
-			return "hii";
+		}
+		if (userServiceImplementation.loginUser(user) == 0) {
+			return new ResponseEntity(new CustomErrorType("Password is incorrect"), HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity(new CustomErrorType("Username doesnt not exist"), HttpStatus.CONFLICT);
 		}
 
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<User> registerUser(@RequestBody User user) throws ClassNotFoundException, SQLException {
+		if (!userServiceImplementation.verifyEmail(user)) {
+			userServiceImplementation.saveUser(user);
+			String token = jwtSecurityConfiguration.tokenGenerator(user);
+			jwtSecurityConfiguration.parseJWT(token);
+			return new ResponseEntity("User successfully registered", HttpStatus.OK);
+			
+		}
+		return new ResponseEntity(new CustomErrorType("Email-id already exist!!"), HttpStatus.CONFLICT);
 	}
 
-
-
+}
